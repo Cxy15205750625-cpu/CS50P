62eec931da1ab15aadc8b9a69ddb202b50222d94
Ω <cxy15205750625@gmail.com>
2026-08-27 20:43:53 +0800

    Implement deep.py

---
+++ deep.py
@@ -0,0 +1,13 @@
+#In deep.py, implement a program that prompts the user for the answer to the Great Question of Life, the Universe and Everything, outputting Yes if the user inputs 42 or (case-insensitively) forty-two or forty two. Otherwise output No
+def main():
+    answer=input("What is the Answer to the Great Question of Life, the Universe, and Everything? ").lower()
+    print(right_answer())
+
+def right_answer(answer):
+    match to:
+        case 42 | "forty two":
+            print("Yes. ")
+        case _:
+            print("No. ")
+
+main()
