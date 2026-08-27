c18779ee9a9c546dbcd3bf0b553ff6da2b28cfa0
Ω <cxy15205750625@gmail.com>
2026-08-27 22:49:18 +0800

    add bank.py

---
+++ bank.py
@@ -0,0 +1,14 @@
+#In a file called bank.py, implement a program that prompts the user for a greeting. If the greeting starts with “hello”, output $0. If the greeting starts with an “h” (but not “hello”), output $20. Otherwise, output $100. Ignore any leading whitespace in the user’s greeting, and treat the user’s greeting case-insensitively.
+def main():
+    greet=input("What? ").lower().strip()
+    greeting(greet)
+
+def greeting(to):
+    if to.startswith("hello"):
+        print("$0")
+    elif to.startswith("h"):
+        print("$20")
+    else:
+        print("$100")
+
+main()
