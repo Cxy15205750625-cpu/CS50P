0cc31d63370bef9985fa878f6791ae5b9a7a6198
Ω <cxy15205750625@gmail.com>
2026-09-17 16:05:29 +0800

    first version

--- outdated.py
+++ outdated.py
@@ -1,11 +1,34 @@
 MONTHS = {
     "January",
-
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
 }
 
 def main():
+    while True:
+        try:
+            x,y,z = input("Date: ").split("/")
+            x = int(x)
+            y = int(y)
 
+            if not z.isdigit() or not z.isalpha():
+                return False
 
-    print()
+        except ValueError:
+            print("please use the format that 'DD/MM/YY'! ")
+
+        else:
+            break
+
+    print(z,f"-x:02-y:02")
 
 main()
