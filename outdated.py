9c9905c045132288c96991fe4d9ae85f64951f93
Ω <cxy15205750625@gmail.com>
2026-09-17 17:06:12 +0800

    change sth

--- outdated.py
+++ outdated.py
@@ -1,34 +1,41 @@
 MONTHS = {
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
+    "January": 1,
+    "February": 2,
+    "March": 3,
+    "April": 4,
+    "May": 5,
+    "June": 6,
+    "July": 7,
+    "August": 8,
+    "September": 9,
+    "October": 10,
+    "November": 11,
+    "December": 12
 }
 
 def main():
     while True:
+        date = input("Date: ").split()
         try:
-            x,y,z = input("Date: ").split("/")
-            x = int(x)
-            y = int(y)
+            if "/" in date:
+                x, y, z = date.split("/")
+                day = int(y)
+                year = int(z)
 
-            if not z.isdigit() or not z.isalpha():
-                return False
+                if not x.isdigit():
+                    month = MONTHS[x]
+                    break
+            else:
+                x, y, z = date.split(" ")
+
+                if not y.endswith(","):
+                    continue
+                day = int(y[:-1])
+                year = int(z)
 
         except ValueError:
             print("please use the format that 'DD/MM/YY'! ")
 
-        else:
-            break
-
-    print(z,f"-x:02-y:02")
+    print("f{month:02}-{day:02}-{year:04}")
 
 main()
